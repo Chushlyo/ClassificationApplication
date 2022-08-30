b904a87cfaac4ab73e8036006443bd89b73e8312
Nikol <1nps1996@gmail.com>
2022-08-30 15:23:29 +0200

    Initialisation

---
+++ src/main/java/com/football/classification/ClassificationApplication.java
@@ -0,0 +1,14 @@
+package com.football.classification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassificationApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Tec");
+//        SpringApplication.run(ClassificationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/football/classification/ClassificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.football.classification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
